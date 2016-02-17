79c79dd5fa14a734d04b16ef0a81affc27354ca4
Joshua King <windosbk+github@gmail.com>
2016-02-17 22:24:16 +1300

    module file dump

---
+++ PulsewayClientPowerShell/PulsewayClientPowerShell.psm1
@@ -0,0 +1,16 @@
+﻿$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+
+Foreach($import in @($Public + $Private))
+{
+    Try
+    {
+        . $import.fullname
+    }
+    Catch
+    {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
+
+Export-ModuleMember -Function $Public.Basename
