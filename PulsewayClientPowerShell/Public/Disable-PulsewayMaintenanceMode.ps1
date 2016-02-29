e247cd4a611a09267187c7ddff687f4c22dec4f7
Joshua King <windosbk+github@gmail.com>
2016-02-29 23:03:35 +1300

    And turn it off again

---
+++ PulsewayClientPowerShell/Public/Disable-PulsewayMaintenanceMode.ps1
@@ -0,0 +1,21 @@
+﻿function Disable-PulsewayMaintenanceMode
+{
+    param
+    (
+        [string[]] $ComputerName
+    )
+
+    foreach ($Target in $ComputerName)
+    {
+        if (Test-PulsewayInstalled -ComputerName $Target)
+        {
+            Invoke-Command -ComputerName $Target -ScriptBlock {
+                Set-ItemProperty -Path "HKLM:\SOFTWARE\MMSOFT Design\PC Monitor" -Name 'MaintenanceMode' -Value 0 -Type String
+            }
+        }
+        else
+        {
+            Write-Warning "No response from $Target"
+        }
+    }
+}
