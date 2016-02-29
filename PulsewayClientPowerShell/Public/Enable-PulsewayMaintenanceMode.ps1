973e88e41d85ff6c9875c5dd29a394bc19a89ce1
Joshua King <windosbk+github@gmail.com>
2016-02-29 22:58:23 +1300

    Powershell enable maintenance mode

---
+++ PulsewayClientPowerShell/Public/Enable-PulsewayMaintenanceMode.ps1
@@ -0,0 +1,21 @@
+﻿function Enable-PulsewayMaintenanceMode
+{
+    param
+    (
+        [string[]] $ComputerName
+    )
+
+    foreach ($Target in $ComputerName)
+    {
+        if (Test-PulsewayInstalled -ComputerName $Target)
+        {
+            Invoke-Command -ComputerName $Target -ScriptBlock {
+                Set-ItemProperty -Path "HKLM:\SOFTWARE\MMSOFT Design\PC Monitor" -Name 'MaintenanceMode' -Value 1 -Type String
+            }
+        }
+        else
+        {
+            Write-Warning "No response from $Target"
+        }
+    }
+}
